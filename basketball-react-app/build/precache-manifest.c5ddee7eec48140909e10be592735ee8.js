047015856442adbeb8f3718e721c19cfbd7b7813
Victor Johnson <vjohnson22@gmail.com>
2019-10-01 19:38:43 -0400

    fixed typo, about to redeploy

---
+++ basketball-react-app/build/precache-manifest.c5ddee7eec48140909e10be592735ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336e14ab563c0032234c2b4e06f6b0ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dde1d28f395b7229fcd",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c36aabdcc07d98a6bb1e",
+    "url": "/static/css/main.005a6c00.chunk.css"
+  },
+  {
+    "revision": "6dde1d28f395b7229fcd",
+    "url": "/static/js/2.4e7b0446.chunk.js"
+  },
+  {
+    "revision": "c36aabdcc07d98a6bb1e",
+    "url": "/static/js/main.f7d2b955.chunk.js"
+  },
+  {
+    "revision": "842147038d13496e1732",
+    "url": "/static/js/runtime~main.6ad42cc6.js"
+  },
+  {
+    "revision": "6e4f17f66028cf61d9c6559983682b6e",
+    "url": "/static/media/NBAmix.6e4f17f6.mp4"
+  }
+]);
